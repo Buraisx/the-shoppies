8ebbbc2efab4d54a4ecd4379e1d8a61588d167ac
Situ William <situ_william@yahoo.com>
2021-01-17 03:58:53 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>The Shoppies</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react';
+
+const SearchBar = props => {
+
+  useEffect(() => {
+
+  }, []);
+
+  return (
+    <div>
+      <input />
+    </div>
+  );
+};
+
+export default SearchBar;
